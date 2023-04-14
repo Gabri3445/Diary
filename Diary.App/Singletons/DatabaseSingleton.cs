0648eaa217692ce22cc7ebcddf26124092163fcb
Gabriele Lopez <gabriele.ope@gmail.com>
2023-04-15 00:45:34 +0200

    Database context singleton

---
+++ Diary.App/Singletons/DatabaseSingleton.cs
@@ -0,0 +1,20 @@
+﻿using Diary.App.Databases;
+
+namespace Diary.App.Singletons;
+
+public static class DatabaseSingleton
+{
+    private static DatabaseContext? _instance;
+    private static readonly object Padlock = new();
+
+    public static DatabaseContext Instance
+    {
+        get
+        {
+            lock (Padlock)
+            {
+                return _instance ??= new DatabaseContext();
+            }
+        }
+    }
+}
