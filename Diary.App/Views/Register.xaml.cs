4662de366234c9f114561b8888b539421ed9c1a2
Gabriele Lopez <gabriele.ope@gmail.com>
2023-04-15 11:34:39 +0200

    Registration viewmodel

---
+++ Diary.App/Views/Register.xaml.cs
@@ -0,0 +1,20 @@
+﻿using System.Windows;
+using Diary.App.ViewModels;
+
+namespace Diary.App.Views;
+
+public partial class Register : Window
+{
+    private readonly RegisterViewModel? _dataContext;
+
+    public Register()
+    {
+        _dataContext = DataContext as RegisterViewModel;
+        InitializeComponent();
+    }
+
+    private void RegisterButton_OnClick(object sender, RoutedEventArgs e)
+    {
+        _dataContext?.InvokeRegisterButtonClicked(this, e);
+    }
+}
